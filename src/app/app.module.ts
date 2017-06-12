f318aeba5be579a87bc9cf995a93cef848601a9d
Nate Genter <ngenter@plasticingenuity.local>
2017-06-12 14:47:13 -0500

    empty project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CalcComponent,
   ],
   imports: [
     BrowserModule,
