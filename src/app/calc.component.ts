5856007bed61ce275a168b186261d30fc9a63012
Nate Genter <ngenter@plasticingenuity.local>
2017-06-14 14:37:29 -0500

    working basic calc

--- src/app/calc.component.ts
+++ src/app/calc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 
 @Component({
   selector: 'calc',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 
 export class CalcComponent {
   
+  name: string;
+  input1: number;
+  input2: number;
+  answer: number = null;
+  total: number; 
+
+  addition(){
+    var total = (this.input1 + this.input2)
+    console.log(total)   
+    this.answer = total  
+  }
+
+  subtraction(){
+    var total = (this.input1 - this.input2)
+    console.log(total)
+    this.answer = total
+  }
+
+  test(){
+    var total = this.input1 - this.input2
+    console.log(total)
+    this.answer = total
+  }
+
+  multiplication(){
+    var total = this.input1 * this.input2
+    console.log(total)
+    this.answer = total
+  }
+
+  division(){
+    var total = this.input1 / this.input2
+    console.log(total)
+    this.answer = total
+  }
+
+  clear(){
+    this.input1 = null;
+    this.input2 = null;
+    this.answer = null
+  }
 }
