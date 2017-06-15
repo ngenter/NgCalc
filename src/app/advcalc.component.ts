0631ee8f67a517ca69b51d2603b51f46982585f2
Brett Hill <bhill@plasticingenuity.com>
2017-06-15 14:03:58 -0500

    run some calcs

--- src/app/advcalc.component.ts
+++ src/app/advcalc.component.ts
@@ -1,29 +1,80 @@
 import { Component } from '@angular/core';
 
+enum MathOperation {
+  Add,
+  Subtract,
+  Multiply,
+  Divide
+}
+
 @Component({
   selector: 'advcalc',
   templateUrl: './advcalc.component.html',
   styles: ['./advcalc.component.css']
 })
-
 export class AdvCalcComponent {
   title = "Advanced Calc"
-  inputNumber: number;
-  firstNumber: number = 0;
-  
-  addition(){
-    console.log("you used the addition method")
-    console.log("this.inputNumber is of value: ", this.inputNumber)
-    console.log("this.inputNumber is of type: ", typeof(this.inputNumber))
-    this.inputNumber = this.firstNumber
-    console.log("firstNumb is of type: " + typeof(this.firstNumber))
 
-    
+  inputNumber: string = "";
 
+  firstNumber: number;
+  secondNumber: number;
+  answer: number;
 
+  operation: MathOperation;
 
-    
+  updateInputNumber(number: string) {
+    this.inputNumber += number;
   }
 
+  setFirstNumber() {
+    this.firstNumber = parseInt(this.inputNumber);
+    this.inputNumber = "";
+  }
 
+  setSecondNumber() {
+    this.secondNumber = parseInt(this.inputNumber);
+    this.inputNumber = "";
+  }
+
+  add() {
+    this.setFirstNumber();
+    this.operation = MathOperation.Add;
+  }
+
+  subtract() {
+    this.setFirstNumber();
+    this.operation = MathOperation.Subtract;
+  }
+
+  multiply() {
+    this.setFirstNumber();
+    this.operation = MathOperation.Multiply;
+  }
+
+  divide() {
+    this.setFirstNumber();
+    this.operation = MathOperation.Divide;
+  }
+
+  doMath() {
+    this.setSecondNumber();
+
+    switch (this.operation) {
+      case MathOperation.Add:
+        this.answer = this.firstNumber + this.secondNumber;
+        break;
+      case MathOperation.Subtract:
+        this.answer = this.firstNumber - this.secondNumber;
+        break;
+      case MathOperation.Multiply:
+        this.answer = this.firstNumber * this.secondNumber;
+        break;
+      case MathOperation.Divide:
+        this.answer = this.firstNumber / this.secondNumber;
+        break;
+      default:
+        break;
+    }
+  }
 }
